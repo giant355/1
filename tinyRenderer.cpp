d7daf7f310304a29c7859a94cba12f140f8876c4
giant <3556183369@qq.com>
2026-05-23 17:43:17 +0800

    初步实现了Blinn-Phong，但是光照计算是有问题的，空间没有统一

--- tinyRenderer.cpp
+++ tinyRenderer.cpp
@@ -7,14 +7,43 @@
 extern mat<4, 4> ModelView, Perspective;
 extern std::vector<double> zbuffer;
 
+constexpr int width = 800;      // output image size
+constexpr int height = 800;
+constexpr vec3    eye{ -1, 0, 2 }; // camera position
+constexpr vec3 center{ 0, 0, 0 }; // camera direction
+constexpr vec3     up{ 0, 1, 0 }; // camera up vector
 
-struct ShaderProgram1 : Shader {
+struct Blinn_PhongShader : Shader {
     TGAColor color{};
+    //vec3 tri[3];
+    Triangle tri;
+    vec3 l;
+    Blinn_PhongShader(const vec3& lightDir) {
+        l=normalized((vec4{lightDir.x,lightDir.y,lightDir.z,0}*ModelTrans*ModelView).xyz());
+    }
     vec4 vertex(const vec4& v) const override {
-        return v *ModelTrans* ModelView * Perspective;
+        vec4 gl_Position = v *ModelTrans * ModelView * Perspective;
+        return gl_Position;
     }
     std::pair<bool, TGAColor> fragment(const vec3 bar)const override {
-        return { false,color };
+        TGAColor gl_FragColor{255,255,255,255};
+        vec3 temp1 = tri[1].xyz() - tri[0].xyz();
+        vec3 temp2 = tri[2].xyz() - tri[1].xyz();
+        vec3 n=normalized(cross(temp1, temp2));//这里假设事先知道缠绕方向
+
+        double ambient = .3;
+        double diff = std::max(0., n * l);
+        vec3 coordinate =
+         mat<3,3>{ {{ tri[0].x,tri[0].y,tri[0].z },
+                    { tri[1].x,tri[1].y,tri[1].z },
+                    { tri[2].x,tri[2].y,tri[2].z }} }*bar;
+        vec3 v = eye - coordinate;
+        vec3 half = normalized(l + v);
+        double spec = std::pow(std::max(half * n, 0.),35);
+        double result = ambient + diff + spec;
+        for (int channel : {0, 1, 2})
+            gl_FragColor[channel] *= std::min(1., ambient + .4 * diff + .9 * spec);
+        return { false, gl_FragColor };
     }
 };
 mat<4, 4> rotation(const vec3& axis, double angle) {
@@ -28,26 +57,20 @@ mat<4, 4> rotation(const vec3& axis, double angle) {
              { 0,                 0,                 0,                 1 }} };
 }
 int main(int argc, char** argv) {
-    constexpr int width = 800;      // output image size
-    constexpr int height = 800;
-    constexpr vec3    eye{ -1, 0, 2 }; // camera position
-    constexpr vec3 center{ 0, 0, 0 }; // camera direction
-    constexpr vec3     up{ 0, 1, 0 }; // camera up vector
-
     lookAt(eye,center,up);
     init_perspective(norm(eye-center));
     init_viewport(0, 0, width, height);
     init_zbuffer(width, height);
-    TGAImage framebuffer(width, height, TGAImage::RGB,{ 177, 195, 209, 255 });
+    TGAImage framebuffer(width, height, TGAImage::RGB);
 
     for (int m = 1; m < argc; m++) {
         Model model(argv[m]);
-        ShaderProgram1 shader;
-        shader.ModelTrans = rotation({1,1,1},3.14/6);
+        Blinn_PhongShader shader({ 1, 1, 1 });
         for (int f = 0; f < model.nfaces(); f++) {
-            shader.color = { static_cast<unsigned char>(std::rand() % 255),static_cast<unsigned char>(std::rand() % 255), static_cast<unsigned char>(std::rand() % 255), 255 };
-            Triangle clip = { shader.vertex(model.vert(f, 0)) ,shader.vertex(model.vert(f, 1)) ,shader.vertex(model.vert(f, 2)) };
-            rasterize(clip, shader, framebuffer);
+            shader.tri[0] = shader.vertex(model.vert(f, 0));
+            shader.tri[1] = shader.vertex(model.vert(f, 1));
+            shader.tri[2] = shader.vertex(model.vert(f, 2));
+            rasterize(shader.tri,shader,framebuffer);
         }
     }
 
