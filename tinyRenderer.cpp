567ef2d7d05a21301ad387e82f4ce9d7afb0ff9a
giant <3556183369@qq.com>
2026-05-28 23:12:53 +0800

    不使用迭代施密特，主要原因是法线贴图上大多数法线都是0，0，1，所以虽然在其他地方n偏移没有影响，但是在明暗交界线处会明显分割

--- tinyRenderer.cpp
+++ tinyRenderer.cpp
@@ -13,14 +13,14 @@ constexpr vec3    eye{ -1, 0, 2 }; // camera position
 constexpr vec3 center{ 0, 0, 0 }; // camera direction
 constexpr vec3     up{ 0, 1, 0 }; // camera up vector
 
-struct Blinn_PhongShader : Shader {
+struct PhongShader : Shader {
     TGAColor color{};
     Model model;
     vec3 l;
     vec2 uv[3];
     vec3 tri[3];
     vec4 triN[3];
-    Blinn_PhongShader(const vec3& lightDir,const Model& m):model(m) {
+    PhongShader(const vec3& lightDir,const Model& m):model(m) {
         l = normalized((ModelView * (ModelTrans * vec4{lightDir.x,lightDir.y,lightDir.z,0})).xyz());//观察空间
     }
     virtual vec4 vertex(const int face,const int vert) {
@@ -33,7 +33,7 @@ struct Blinn_PhongShader : Shader {
         return Perspective*v ;//裁剪空间
     }
     std::pair<bool, TGAColor> fragment(const vec3 bar)const override {
-        vec2 barUV = uv[0] * bar.x + uv[1] * bar.y + uv[2] * bar.z;
+        vec2 barUV =  uv[0] * bar.x +   uv[1] * bar.y +   uv[2] * bar.z;
         vec4 barN = triN[0] * bar.x + triN[1] * bar.y + triN[2] * bar.z;
         vec3 N = normalized((ModelView * ModelTrans.invert_transpose() * barN).xyz());
 
@@ -43,25 +43,13 @@ struct Blinn_PhongShader : Shader {
                     {tri[2]- tri[0]}} };
         mat<2, 3>TB = U.invert() * E;
 
-        //balanced Gram-Schmidt正交化
         vec3 T = TB[0],B = TB[1];
-        double factor = .25;              
-        for (int i = 0; i < 10; ++i) {                                                             
-            T = normalized(T);
-            N = normalized(N);
-            B = normalized(B);
-
-            vec3 T1 = T - factor * ((T * N) / (N * N)) * N - factor * ((T * B) / (B * B)) * B;     
-            vec3 B1 = B - factor * ((B * T) / (T * T)) * T - factor * ((B * N) / (N * N)) * N;     
-            vec3 N1 = N - factor * ((N * T) / (T * T)) * T - factor * ((N * B) / (B * B)) * B;     
-
-            T = T1;                                                                                
-            N = N1;                                                                                
-            B = B1;                                                                                
-        }                                     
+        T = normalized(T);
+        N = normalized(N);
+        B = normalized(B);
         T = T - ((T * N) / (N * N)) * N;                                                           
         B = cross(N,T);
-        mat<3, 3>TBN{T,B,N};
+        mat<3, 3>TBN=mat<3,3>{T,B,N}.transpose();
        
         vec3 n = TBN*model.normal(barUV).xyz();
 
@@ -97,10 +85,10 @@ int main(int argc, char** argv) {
     for (int m = 1; m < argc; m++) {
         Model model(argv[m]);
         std::cerr << argv[m]<<std::endl;
-        Blinn_PhongShader shader({ -1, 1, 1 }, model);
+        PhongShader shader({ 1, 1, 1 }, model);
         std::string fname(argv[m]);
-        //if (fname.find("eye_outer") != std::string::npos)
-            //shader.ModelTrans = shader.ModelTrans*mat<4,4>{ {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0.1,1}} };//修复bug
+        if (fname.find("eye_outer") != std::string::npos)
+            shader.ModelTrans = shader.ModelTrans*mat<4,4>{ {{1,0,0,0}, {0,1,0,0}, {0,0,1,0}, {0,0,0.1,1}} };//修复bug
         for (int i = 0; i < model.nfaces(); i++) {
             Triangle clip = {shader.vertex(i, 0), shader.vertex(i, 1), shader.vertex(i, 2)};
             rasterize(clip, shader, framebuffer);
